c70324cbe79aadd674422aabebab228ee96319a1
Clémence Le Roux <clemence.le-roux@ecole.ensicaen.fr>
2022-06-23 15:54:03 +0200

    start

--- back/fer_backend/paths/paths.py
+++ back/fer_backend/paths/paths.py
@@ -5,3 +5,8 @@ bpapi = Blueprint('api/v1', __name__, url_prefix='/api/v1')
 @bpapi.route("/")
 def home():
     return "Accueil"
+
+@bpapi.route('/upload', methods=['GET','POST'])
+def upload():
+    #Get the image
+    return "TODO : GET PICTURE"
