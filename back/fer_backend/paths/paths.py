cecba6dd6bf9da9aae07a53146fd0d1439ed1956
Clémence Le Roux <45121115+SakuraIsana@users.noreply.github.com>
2022-06-24 11:40:04 +0200

    Update paths.py

--- back/fer_backend/paths/paths.py
+++ back/fer_backend/paths/paths.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, request, redirect, jsonify
+from .ia_treatments import test_image
 
 bpapi = Blueprint('api/v1', __name__, url_prefix='/api/v1')
 
 @bpapi.route("/")
 def home():
     return "Accueil"
+
+@bpapi.route("/upload")
+def upload():
+    #Get the image
+    #Convert the image
+    #Make the prediction test_image(picture)
+    return "TODO : GET PICTURE"
