c29f648931a2715bb1e7bdf7365ed269c1f5c067
Antonin Huaut <antonin.huaut@gmail.com>
2022-06-27 14:25:39 +0200

    Improve help

--- front/src/lib/components/Help.svelte
+++ front/src/lib/components/Help.svelte
@@ -2,11 +2,13 @@
   import { help } from "$lib/stores/preference_store"
 </script>
 
-<div>
-  <p class="fs-5">
+<div class="w-50 mx-auto">
+  <p class="fs-6 text-start">
     When using image upload or webcam, it is imperative that there is only your
-    face with as little background as possible.
-    <br />In addition, you should not wear accessories (glasses, ...)
+    face with as little background as possible and the image must be square
+    (same width/height).
+    <br />If you use a webcam, the picture will be cropped from the center.
+    <br />In addition, you should not wear accessories (glasses, ...).
   </p>
 
   <a href={"#"} class="btn btn-primary" on:click={() => help.set(false)}>
