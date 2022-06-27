29ae75dae3411bdf47bf3757d2af4231822df60f
Antonin Huaut <antonin.huaut@gmail.com>
2022-06-27 14:20:55 +0200

    Help

--- front/src/lib/components/Help.svelte
+++ front/src/lib/components/Help.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div>
-  <h1>TODO help menu</h1>
+  <p class="fs-5">
+    When using image upload or webcam, it is imperative that there is only your
+    face with as little background as possible.
+    <br />In addition, you should not wear accessories (glasses, ...)
+  </p>
 
   <a href={"#"} class="btn btn-primary" on:click={() => help.set(false)}>
     I understand, let me try!
